ba0cbb7227dc4bc6516e214e6dac71a4a2383558
Geno Racklin Asher <geno.racklinasher@gmail.com>
2021-10-17 23:26:29 +0100

    Use HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Http\Resources\RoomResource;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         RoomResource::withoutWrapping();
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }        
     }
 }
